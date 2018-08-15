dd27bc8cb395c819ece3f0d020623a872e575c94
KimSangYun <32999975+UnixTeamAdmin@users.noreply.github.com>
2018-08-15 10:41:24 +0900

    Add files via upload

---
+++ DistanceHack.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+* @name DistanceHack
+* @main DistanceHack\Main
+* @author BLEAN
+* @version 1.0
+* @api 4.0.0
+*/
+
+namespace DistanceHack;
+
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+use pocketmine\Player;
+
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
+
+	public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	/**
+	* @param EntityDamageEvent $event
+	*
+	* @priority LOWEST
+	*/
+	public function onDamage(EntityDamageEvent $event){
+		if (!$event instanceof EntityDamageByEntityEvent)
+		return true;
+		if (!$event->getDamager() instanceof Player)
+		return true;
+		if (!$event->getEntity() instanceof Player)
+		return true;
+		if ($event->getEntity()->distanceSquared($event->getDamager()) > 20){
+			$this->getLogger()->info($event->getDamager()->getName() . ', 사거리 거리 ' . (int) ($event->getEntity()->distanceSquared($event->getDamager()) / 4) . ' 블럭');
+			$event->getDamager()->sendPopUp('§c사거리 핵 감지로 캔슬 되었습니다');
+			$event->setCancelled();
+		}
+	}
+
+}
